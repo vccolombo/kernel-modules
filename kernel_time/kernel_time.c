3706f82a7dbb9e8556938ff15f46520c0391cd6a
Victor Cora Colombo <victorcora98@gmail.com>
2021-10-03 10:44:07 -0300

    kernel_time: make code style more close to Kernel's

--- kernel_time/kernel_time.c
+++ kernel_time/kernel_time.c
@@ -1,13 +1,13 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
+#include <linux/cdev.h> // cdev
 #include <linux/device.h>
+#include <linux/fs.h> // register_chrdev_region, alloc_chrdev_region
+#include <linux/init.h>
 #include <linux/kdev_t.h> // MAJOR, MKDEV
-#include <linux/fs.h>     // register_chrdev_region, alloc_chrdev_region
-#include <linux/cdev.h>   // cdev
-#include <linux/types.h>  // dev_t
+#include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/rtc.h>
 #include <linux/slab.h>
+#include <linux/types.h> // dev_t
 
 #define MODULE_NAME "kernel_time"
 #define MODULE_LOG_START MODULE_NAME ": "
@@ -23,27 +23,29 @@ dev_t devno;
 
 static char result[MAX_SZ + 1];
 
-ssize_t kernel_time_read(struct file *filp, char __user *buf, size_t count, loff_t *off)
+ssize_t kernel_time_read(struct file *filp, char __user *buf, size_t count,
+			 loff_t *off)
 {
-    struct timespec64 time;
-    struct rtc_time tm;
-    unsigned long local_time;
-    unsigned short len;
+	struct timespec64 time;
+	struct rtc_time tm;
+	unsigned long local_time;
+	unsigned short len;
 
-    if (*off == 0)
-    {
-        ktime_get_real_ts64(&time);
-        local_time = (u32)(time.tv_sec - (sys_tz.tz_minuteswest * 60));
-        rtc_time64_to_tm(local_time, &tm);
+	if (*off == 0) {
+		ktime_get_real_ts64(&time);
+		local_time = (u32)(time.tv_sec - (sys_tz.tz_minuteswest * 60));
+		rtc_time64_to_tm(local_time, &tm);
 
-        snprintf(result, MAX_SZ, "%04d-%02d-%02d %02d:%02d:%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
-                 tm.tm_hour, tm.tm_min, tm.tm_sec);
-    }
+		snprintf(result, MAX_SZ, "%04d-%02d-%02d %02d:%02d:%02d",
+			 tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
+			 tm.tm_hour, tm.tm_min, tm.tm_sec);
+	}
 
-    len = strlen(result); // it is not necessary to repeat this every time, as it is always the same value
-    count = simple_read_from_buffer(buf, count, off, result, len);
+	len = strlen(result); // it is not necessary to repeat this every time,
+			      // as it is always the same value
+	count = simple_read_from_buffer(buf, count, off, result, len);
 
-    return count;
+	return count;
 }
 
 static struct file_operations kernel_time_fops = {
@@ -53,69 +55,66 @@ static struct file_operations kernel_time_fops = {
 
 static int kernel_time_setup_device(void)
 {
-    int err;
+	int err;
 
-    cdev_init(&cdev, &kernel_time_fops);
-    cdev.owner = THIS_MODULE;
-    err = cdev_add(&cdev, devno, 1);
+	cdev_init(&cdev, &kernel_time_fops);
+	cdev.owner = THIS_MODULE;
+	err = cdev_add(&cdev, devno, 1);
 
-    return err;
+	return err;
 }
 
 static int __init kernel_time_init(void)
 {
-    int err;
-    struct device *dev_ret;
+	int err;
+	struct device *dev_ret;
 
-    err = alloc_chrdev_region(&devno, 0, 1, MODULE_NAME);
-    if (unlikely(err < 0))
-    {
-        printk(KERN_ERR MODULE_LOG_START "unable to alloc major number\n");
-        goto out;
-    }
+	err = alloc_chrdev_region(&devno, 0, 1, MODULE_NAME);
+	if (unlikely(err < 0)) {
+		pr_err(MODULE_LOG_START "unable to alloc major number\n");
+		goto out;
+	}
 
-    if (IS_ERR(cl = class_create(THIS_MODULE, MODULE_NAME)))
-    {
-        err = PTR_ERR(cl);
-        goto unregister_chrdev;
-    }
+	if (IS_ERR(cl = class_create(THIS_MODULE, MODULE_NAME))) {
+		err = PTR_ERR(cl);
+		goto unregister_chrdev;
+	}
 
-    if (IS_ERR(dev_ret = device_create(cl, NULL, devno, NULL, MODULE_NAME)))
-    {
-        err = PTR_ERR(dev_ret);
-        goto class_destroy;
-    }
+	if (IS_ERR(dev_ret =
+		       device_create(cl, NULL, devno, NULL, MODULE_NAME))) {
+		err = PTR_ERR(dev_ret);
+		goto class_destroy;
+	}
 
-    err = kernel_time_setup_device();
-    if (unlikely(err < 0))
-    {
-        printk(KERN_ERR MODULE_LOG_START "unable to setup device\n");
-        goto device_destroy;
-    }
+	err = kernel_time_setup_device();
+	if (unlikely(err < 0)) {
+		pr_err(MODULE_LOG_START "unable to setup device\n");
+		goto device_destroy;
+	}
 
-    printk(KERN_INFO MODULE_LOG_START "registered with major %d and minor %d\n",
-           MAJOR(devno), MINOR(devno));
+	pr_info(MODULE_LOG_START "registered with major %d and minor %d\n",
+		MAJOR(devno), MINOR(devno));
 
-    return 0;
+	return 0;
 
 device_destroy:
-    device_destroy(cl, devno);
+	device_destroy(cl, devno);
 class_destroy:
-    class_destroy(cl);
+	class_destroy(cl);
 unregister_chrdev:
-    unregister_chrdev_region(devno, 1);
+	unregister_chrdev_region(devno, 1);
 out:
-    return err;
+	return err;
 }
 
 static void __exit kernel_time_exit(void)
 {
-    cdev_del(&cdev);
-    device_destroy(cl, devno);
-    class_destroy(cl);
-    unregister_chrdev_region(devno, 1);
+	cdev_del(&cdev);
+	device_destroy(cl, devno);
+	class_destroy(cl);
+	unregister_chrdev_region(devno, 1);
 
-    printk(KERN_INFO MODULE_LOG_START "unregistered");
+	pr_info(MODULE_LOG_START "unregistered");
 }
 
 module_init(kernel_time_init);
